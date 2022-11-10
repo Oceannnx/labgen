4008f7b89b756325601c1ac7f76dd4f9cb6bfac2
หนมจีบ <111548711+Oceannnx@users.noreply.github.com>
2022-11-10 23:40:47 +0700

    Update lebgen_1.py

--- lebgen_1.py
+++ lebgen_1.py
@@ -1,8 +1,6 @@
 def gen5odds():
     for i in range(0,100,10):
-        yield list(i+j for j in range(10) if j%2==1 )
+        yield list(i+j for j in range(10) if j%2==1)
  
 def Problem1(): 
-    genodd = gen5odds()
-    genoddsum = [sum(i) for i in gen5odds()]
-    return genoddsum
+    return [sum(i) for i in gen5odds()]
